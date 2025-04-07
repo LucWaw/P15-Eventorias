6d876fe780cd9ea4b769833e609204d216d40116
Lucas Bigeard <luc.allau@gmail.com>
2025-04-07 18:42:53 +0200

    test: enhance AddEventTest with detailed logging for better traceability

--- app/src/androidTest/java/com/openclassrooms/eventorias/AddEventTest.kt
+++ app/src/androidTest/java/com/openclassrooms/eventorias/AddEventTest.kt
@@ -29,15 +29,21 @@ class AddEventTest : KoinTest {
     @Test
     fun testAddEvent() {
 
+        println("testAddEvent : Start")
+
         composeTestRule.waitUntil(timeoutMillis = 50000) {
             composeTestRule
                 .onNodeWithText("Sign in with email")
                 .isDisplayed()
         }
 
+        println("testAddEvent : Sign in with email displayed")
+
         composeTestRule
             .onNodeWithText("Sign in with email")
             .performClick()
+
+        println("testAddEvent : Sign in with email clicked")
 
         composeTestRule.waitUntil(timeoutMillis = 50000) {
             composeTestRule
@@ -45,9 +51,13 @@ class AddEventTest : KoinTest {
                 .isDisplayed()
         }
 
+        println("testAddEvent : Already have an account? Login displayed")
+
         composeTestRule
             .onNodeWithText("Already have an account? Login")
             .performClick()
+
+        println("testAddEvent : Already have an account? Login clicked")
 
         composeTestRule.waitUntil(timeoutMillis = 50000) {
             composeTestRule
@@ -55,18 +65,26 @@ class AddEventTest : KoinTest {
                 .isDisplayed()
         }
 
+        println("testAddEvent : Please enter your password. displayed")
+
         composeTestRule
             .onNodeWithTag("emailInput")
             .performTextInput("fakehhkgugugugufugubkdt@mail.com")
+
+        println("testAddEvent : emailInput filled")
 
         composeTestRule
             .onNodeWithTag("passwordInput")
             .performTextInput("Gjgjgjvkfyfuvk")
 
+        println("testAddEvent : passwordInput filled")
+
 
         composeTestRule
             .onNodeWithTag("logInButton")
             .performClick()
+
+        println("testAddEvent : logInButton clicked")
 
         // WAIT Loading
         composeTestRule.waitUntil(timeoutMillis = 50000) {
@@ -75,58 +93,82 @@ class AddEventTest : KoinTest {
                 .isDisplayed()
         }
 
+        println("testAddEvent : Add an Event displayed")
+
         composeTestRule.waitUntil(timeoutMillis = 10000) {
             composeTestRule
                 .onAllNodesWithTag("eventItemTag")
                 .fetchSemanticsNodes().isNotEmpty()
         }
 
+        println("testAddEvent : eventItemTag displayed")
+
         val initialEventCount = composeTestRule
             .onAllNodesWithTag("eventItemTag")
             .fetchSemanticsNodes()
             .size
 
+
+
         composeTestRule.onAllNodesWithTag("eventItemTag").printToLog("eventItemTag")
+
+        println("testAddEvent : eventItemTag count : $initialEventCount")
 
 
         composeTestRule
             .onNodeWithContentDescription("Add an Event")
             .performClick()
 
+        println("testAddEvent : Add an Event clicked")
+
         composeTestRule
             .onNodeWithTag("eventTitleInput")
             .performTextInput("Nouvel Événement")
+
+        println("testAddEvent : eventTitleInput filled")
 
         composeTestRule
             .onNodeWithTag("eventDescriptionInput")
             .performTextInput("Ceci est un test")
 
+        println("testAddEvent : eventDescriptionInput filled")
+
         composeTestRule
             .onNodeWithTag("eventAddressInput")
             .performTextInput("Paris")
 
+        println("testAddEvent : eventAddressInput filled")
 
         composeTestRule.onNodeWithTag("eventDateInput")
             .performClick()
 
+        println("testAddEvent : eventDateInput clicked")
+
         //Click on the "OK" button
         composeTestRule
             .onNodeWithText("OK")
             .performClick()
 
+
+        println("testAddEvent : OK clicked")
 
         composeTestRule.onNodeWithTag("eventTimeInput")
             .performClick()
 
+        println("testAddEvent : eventTimeInput clicked")
+
         //Click on the "OK" button
         composeTestRule
             .onNodeWithText("OK")
             .performClick()
 
+        println("testAddEvent : OK clicked")
 
         composeTestRule
             .onNodeWithText("Validate")
             .performClick()
+
+        println("testAddEvent : Validate clicked")
 
         composeTestRule.waitUntil(timeoutMillis = 50000) {
             composeTestRule
@@ -135,13 +177,15 @@ class AddEventTest : KoinTest {
         }
 
 
-
+        println("testAddEvent : Add an Event displayed")
 
         composeTestRule.waitUntil(timeoutMillis = 50000) {
             composeTestRule
                 .onAllNodesWithTag("eventItemTag")
                 .fetchSemanticsNodes().size > initialEventCount
         }
+
+        println("testAddEvent : eventItemTag displayed")
 
 
         val newEventCount = composeTestRule
